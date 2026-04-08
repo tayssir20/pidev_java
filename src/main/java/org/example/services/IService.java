c4592bc33be8929f589101d93c8c2249efdf597f
aziz rajhi <rajhiaziz2@gmail.com>
2026-04-08 23:56:42 +0100

    correction

--- src/main/java/org/example/services/IService.java
+++
@@ -1,9 +0,0 @@
-package org.example.services;
-import java.sql.SQLException;
-import java.util.List;
-public interface IService<Objet> {
-    void ajouter(Objet objet) throws SQLException;
-    void modifier(Objet objet) throws SQLException;
-    void supprimer(int id)throws SQLException;
-    List<Objet> getAll() throws SQLException;
-}
